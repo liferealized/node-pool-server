02f6544c5d240bc728b85757bf54b06506795c50
James Gibson <me@iamjamesgibson.com>
2012-10-09 14:15:56 -0400

    added some diff targets to the example config for others to use

--- example.config.js
+++ example.config.js
@@ -18,6 +18,11 @@ var config = {
 		, maxsize: 10485760
 	}
 	// getwork settings
+	// some optional diff targets while i write the code to calculate these
+	// diff  1 = ffffffffffffffffffffffffffffffffffffffffffffffffffffffff00000000
+	// diff  2 = 0000000000000000000000000000000000000000000000000000008000000000
+	// diff  5 = 0000000000000000000000000000000000000000333333333333333300000000
+	// diff 10 = 0000000000000000000000000000000000000000999999999999991900000000
 	, getwork: {
 		  target: 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffff00000000'
 		, submitold: true
